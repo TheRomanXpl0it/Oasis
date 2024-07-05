241c7ba3601e59662c4b1cf791e7890ffe6cd3ca
tde-nico <101901057+tde-nico@users.noreply.github.com>
2024-07-05 21:21:17 +0200

    routes fix

--- router/entry.sh
+++ router/entry.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-ifaces=$(ip link show | sed -n 's/.*\(eth.*\)@.*/\1/p')
-
-for iface in $ifaces; do
-	iptables -A FORWARD -i $iface -j ACCEPT
-	iptables -t nat -A POSTROUTING -o $iface -j MASQUERADE
-done
+iptables -A FORWARD -i eth+ -j ACCEPT
+iptables -t nat -A POSTROUTING -o eth+ -j MASQUERADE
 
 tail -f /dev/null
--- vm/entry.sh
+++ vm/entry.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-ip route add 10.10.0.0/24 via 10.60.200.200
-
 ip link set eth0 down
 ip link set eth0 name game
 ip link set game up
+
+ip route add 10.10.0.0/24 via 10.60.200.200
 
 find . -maxdepth 1 -mindepth 1 -type d -exec docker compose -f {}/compose.yml up -d \;
 
