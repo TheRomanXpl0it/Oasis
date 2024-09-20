025e5adb131640d5d3c395e199f7ad1aa593aee2
Domingo Dirutigliano <me@domy.sh>
2024-09-20 23:31:40 +0200

    add: init frontend project + removed old scoreboard

--- game_server/src/scoreboard.go
+++ game_server/src/scoreboard.go
@@ -1,43 +1,17 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"database/sql"
 	"encoding/json"
-	"fmt"
 	"game/db"
 	"game/log"
-	"html/template"
-	"net"
 	"net/http"
 	"os"
 	"path/filepath"
-	"sort"
 	"time"
 
 	"github.com/gorilla/mux"
 )
-
-/*
-{
-	"service": "service1",
-	"stolen_flags": 0,
-	"lost_flags": 0,
-	"sla": 100.0,
-	"score": 0.0,
-	"ticks_up": 0,
-	"ticks_down": 0,
-	"put_flag": 101,
-	"put_flag_msg": "msg",
-	"get_flag": 101,
-	"get_flag_msg": "msg",
-	"sla_check": 101,
-	"sla_check_msg": "msg",
-	"final_score": 0.0,
-}
-
-*/
 
 type ServiceRoundStatus struct {
 	Service     string  `json:"service"`
@@ -56,34 +30,10 @@ type ServiceRoundStatus struct {
 	FinalScore  float64 `json:"final_score"`
 }
 
-/*
-{
-	"team": "10.60.0.1",
-	"score": 0.0,
-}
-*/
-
 type TeamRoundStatusShort struct {
 	Team  string  `json:"team"`
 	Score float64 `json:"score"`
 }
-
-/*
-/api/chart
-[
-	{
-		"round": 1,
-		"scores": [
-			{
-				"team": "10.60.0.1",
-				"score": 0.0,
-			},
-			{...}
-		]
-	},
-	{...}
-]
-*/
 
 type ChartAPIResponse struct {
 	Round  uint                   `json:"round"`
@@ -139,38 +89,6 @@ func handleChart(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
-
-/*
-/api/scoreboard
-{
-
-	"round": 1,
-	"scores": [
-		{
-			"team": "10.60.0.1",
-			"image": "url",
-			"score": 0.0,
-			"services": [
-				{
-					"service": "service1",
-					"stolen_flags": 0,
-					"lost_flags": 0,
-					"sla": 100.0,
-					"score": 0.0,
-					"ticks_up": 0,
-					"ticks_down": 0,
-					"put_flag": 101,
-					"put_flag_msg": "msg",
-					"get_flag": 101,
-					"get_flag_msg": "msg",
-					"sla_check": 101,
-					"sla_check_msg": "msg",
-				}
-			]
-		}
-	]
-}
-*/
 
 type TeamRoundStatus struct {
 	Team     string               `json:"team"`
@@ -249,38 +167,6 @@ func handleScoreboard(w http.ResponseWriter, r *http.Request) {
 
 }
 
-/*
-/api/team/{team}
-[
-	{
-
-		"round": 1,
-		"score": {
-			"team": "10.60.0.1",
-			"image": "url",
-			"score": 0.0,
-			"services": [
-				{
-					"service": "service1",
-					"stolen_flags": 0,
-					"lost_flags": 0,
-					"sla": 100.0,
-					"score": 0.0,
-					"ticks_up": 0,
-					"ticks_down": 0,
-					"put_flag": 101,
-					"put_flag_msg": "msg",
-					"get_flag": 101,
-					"get_flag_msg": "msg",
-					"sla_check": 101,
-					"sla_check_msg": "msg",
-				}
-			]
-		}
-	}
-]
-*/
-
 type TeamAPIResponse struct {
 	Round uint            `json:"round"`
 	Score TeamRoundStatus `json:"score"`
@@ -352,30 +238,6 @@ func handleTeam(w http.ResponseWriter, r *http.Request) {
 
 }
 
-/*
-/api/config
-{
-	"teams":[
-		{
-			"name": "team1",
-			"host": "10.60.0.1",
-			"image": "url",
-			"nop": false
-		},{...}
-	]
-	"services":[
-		"service1",
-		"service2",
-		...
-	],
-	"start_time": "",
-	"round_len": 60000,
-	"flag_expire_ticks": 5,
-	"submitter_flags_limit": 2000,
-	"submitter_rate_limit": 10000,
-}
-*/
-
 type TeamConfig struct {
 	Name  string `json:"name"`
 	Host  string `json:"host"`
@@ -418,105 +280,11 @@ func handleConfig(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// spaHandler implements the http.Handler interface, so we can use it
-// to respond to HTTP requests. The path to the static directory and
-// path to the index file within that static directory are used to
-// serve the SPA in the given static directory.
 type spaHandler struct {
 	staticPath string
 	indexPath  string
 }
 
-type TemplService struct {
-	Name          string
-	StolenFlags   uint
-	LostFlags     uint
-	Score         string
-	TicksUp       uint
-	TicksDown     uint
-	Sla           string
-	LastTickColor string
-	LastTick      bool
-}
-
-type TemplTeam struct {
-	Team       string
-	Services   map[string]TemplService
-	TotalScore string
-}
-
-func handleOldScoreboard(w http.ResponseWriter, r *http.Request) {
-	var ctx context.Context = context.Background()
-	tmpl := template.Must(template.ParseFiles("templates/index.html"))
-
-	realIPs := make([]net.IP, 0, len(conf.Teams))
-
-	for ip := range conf.Teams {
-		realIPs = append(realIPs, net.ParseIP(ip))
-	}
-
-	sort.Slice(realIPs, func(i, j int) bool {
-		return bytes.Compare(realIPs[i], realIPs[j]) < 0
-	})
-
-	sortedTeams := make([]string, len(realIPs))
-
-	for i, ip := range realIPs {
-		sortedTeams[i] = ip.String()
-	}
-
-	teams := map[string][]TemplTeam{
-		"Teams": make([]TemplTeam, 0, len(conf.Teams)),
-	}
-
-	for _, team := range sortedTeams {
-		t := TemplTeam{
-			Team:       team,
-			Services:   make(map[string]TemplService),
-			TotalScore: "0",
-		}
-		round := db.GetExposedRound()
-		var totScore float64 = 0.0
-		for _, service := range conf.Services {
-			lastScore := new(db.StatusHistory)
-			lastScore.CheckStatus = ERROR
-
-			if err := conn.NewSelect().Model(lastScore).Where("team = ? and service = ? and round = ?", team, service, round).Scan(ctx); err != nil {
-				if err != sql.ErrNoRows {
-					log.Panicf("Error fetching last SLA check: %v", err)
-				}
-			}
-			lastTickColor := "red"
-			if lastScore.CheckStatus == OK && lastScore.PutFlagStatus == OK && lastScore.GetFlagStatus == OK {
-				lastTickColor = "green"
-			}
-
-			score := lastScore.Sla * float64(lastScore.Score)
-			t.Services[service] = TemplService{
-				Name:          service,
-				StolenFlags:   lastScore.StolenFlags,
-				LostFlags:     lastScore.LostFlags,
-				Score:         fmt.Sprintf("%.2f", score),
-				TicksUp:       lastScore.SlaUpTimes,
-				TicksDown:     lastScore.SlaTotTimes - lastScore.SlaUpTimes,
-				Sla:           fmt.Sprintf("%.2f", lastScore.Sla*100),
-				LastTickColor: lastTickColor,
-				LastTick:      lastTickColor == "green",
-			}
-			totScore += score
-		}
-		t.TotalScore = fmt.Sprintf("%.2f", totScore)
-		teams["Teams"] = append(teams["Teams"], t)
-
-	}
-
-	tmpl.Execute(w, teams)
-}
-
-// ServeHTTP inspects the URL path to locate a file within the static dir
-// on the SPA handler. If a file is found, it will be served. If not, the
-// file located at the index path on the SPA handler will be served. This
-// is suitable behavior for serving an SPA (single page application).
 func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// Join internally call path.Clean to prevent directory traversal
 	path := filepath.Join(h.staticPath, r.URL.Path)
@@ -547,11 +315,10 @@ func serveScoreboard() {
 	router.HandleFunc("/api/chart", handleChart).Methods("GET")
 	router.HandleFunc("/api/team/{team_id}", handleTeam).Methods("GET")
 	router.HandleFunc("/api/config", handleConfig).Methods("GET")
-	router.HandleFunc("/", handleOldScoreboard).Methods("GET")
 
 	log.Noticef("Starting flag_ids server on :80")
 	spa := spaHandler{staticPath: "frontend", indexPath: "index.html"}
-	router.PathPrefix("/new/").Handler(spa)
+	router.PathPrefix("/").Handler(spa)
 
 	srv := &http.Server{
 		Handler:      router,
